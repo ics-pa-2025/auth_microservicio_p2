62225f0154dbae2d57bf2c7657fe2cfc12d5ae89
MartinCesano <cesano305@gmail.com>
2025-10-12 09:58:59 +0000

    Prueba de nueva configuracion de cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './config/database.config';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 import { RbacSeeder } from './seeder/rbac.seeder';
-import { corsConfig } from './config/cors.config';
-import * as cors from 'cors';
 
 @Module({
     imports: [
@@ -26,12 +24,4 @@ import * as cors from 'cors';
     controllers: [AppController],
     providers: [AppService, RbacSeeder],
 })
-export class AppModule implements NestModule {
-    constructor(private readonly configService: ConfigService) {} // inyectamos ConfigService
-
-    configure(consumer: MiddlewareConsumer) {
-        consumer
-            .apply(cors(corsConfig(this.configService))) // <-- pasamos la instancia
-            .forRoutes('*');
-    }
-}
+export class AppModule {}
